30035198497a1bd6b156055c2aa628300639d82e
Renexo <35356839+Renexo-git@users.noreply.github.com>
2021-05-25 22:27:16 -0300

    Update render.sh

--- usr/share/shml/render.sh
+++ usr/share/shml/render.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Content-type: text/html"
 echo ""
-awk -f "./parser.awk" $DOCUMENT_ROOT$DOCUMENT_URI $@
+$DOCUMENT=$DOCUMENT_ROOT$DOCUMENT_URI
+if test -f "$DOCUMENT"; then
+    awk -f "./parser.awk" $DOCUMENT $@
+else
+    echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><hr><p>The requested URL was not found on this server.</p></body></html>'
+fi
